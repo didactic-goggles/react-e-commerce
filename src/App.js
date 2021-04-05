6883d0fc63a608e6032ed125197cf5dd4f917175
didactic-goggles <58259532+developerd1320@users.noreply.github.com>
2021-04-06 02:12:23 +0300

    PerfectScrollbar removed

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import './assets/css/main.css';
 import './assets/js/index';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'react-perfect-scrollbar/dist/css/styles.css';
-import PerfectScrollbar from 'react-perfect-scrollbar';
+// import PerfectScrollbar from 'react-perfect-scrollbar';
 import { AuthProvider } from './context';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import Footer from './components/Footer';
 
 const App = () => {
-  const scrollElement = useRef();
-  setTimeout(
-    () => (scrollElement ? (window.scrollElement = scrollElement.current) : ''),
-    1000
-  );
+  // const scrollElement = useRef();
+  // setTimeout(
+  //   () => (scrollElement ? (window.scrollElement = scrollElement.current) : ''),
+  //   1000
+  // );
 
   return (
     <AuthProvider>
       <Router>
-        <PerfectScrollbar ref={scrollElement}>
+        <Header />
+        <Main />
+        <Footer />
+        {/* <PerfectScrollbar ref={scrollElement}>
           <Header />
           <Main />
           <Footer />
-        </PerfectScrollbar>
+        </PerfectScrollbar> */}
       </Router>
     </AuthProvider>
   );
