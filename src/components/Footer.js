09cd5174b9dc6c8b2d80a485390ce1a249c51068
didactic-goggles <58259532+developerd1320@users.noreply.github.com>
2021-04-11 15:01:21 +0300

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,49 +3,67 @@ import { NavLink } from 'react-router-dom';
 import { FaMapMarked, FaEnvelope, FaPhoneAlt } from 'react-icons/fa';
 const Footer = () => {
   return (
-    <footer className="bg-dark mt-auto text-white py-3">
+    <footer className="bg-dark mt-auto text-white py-5">
       <div className="container">
-        <div className="row align-items-center">
-          <div className="col-md-4">
+        <div className="d-flex flex-column flex-md-row justify-content-center">
+          <div
+            className="border-right-md border-sm-0 flex-fill px-3 d-flex align-items-center mb-3"
+            ref={(el) =>
+              el &&
+              el.style.setProperty('border-color', '#6c757d3d', 'important')
+            }
+          >
             <h5>Logo</h5>
           </div>
-          <div className="col-md-4">
-            <ul className="nav flex-column">
-              <li className="nav-item px-3">
-                <h5>Linkler</h5>
-              </li>
+          <div
+            className="border-right-md border-sm-0 flex-fill px-0 px-md-3 mb-3"
+            ref={(el) =>
+              el &&
+              el.style.setProperty('border-color', '#6c757d3d', 'important')
+            }
+          >
+            <h5 className="px-3">Linkler</h5>
+            <ul className="nav flex-row">
               <li className="nav-item">
                 <NavLink to="/" className="nav-link text-white">
                   Anasayfa
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to="/" className="nav-link text-white">
+                <NavLink to="/kurumsal" className="nav-link text-white">
                   Kurumsal
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to="/" className="nav-link text-white">
+                <NavLink to="/urunler" className="nav-link text-white">
                   Ürünler
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to="/" className="nav-link text-white">
+                <NavLink to="/diger" className="nav-link text-white">
                   Diğer
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/iletisim" className="nav-link text-white">
+                  İletişim
                 </NavLink>
               </li>
             </ul>
           </div>
-          <div className="col-md-4">
+          <div className="flex-fill px-3">
             <div className="d-flex flex-column">
               <span className="mb-2">
-                <FaMapMarked className="me-2" style={{fontSize: '25px'}}/> Adres: Ssdsdsd SDSDSDsd sdsds
+                <FaMapMarked className="me-2" style={{ fontSize: '25px' }} />{' '}
+                Adres: Ssdsdsd SDSDSDsd sdsds
               </span>
               <span className="mb-2">
-                <FaPhoneAlt className="me-2" style={{fontSize: '25px'}}/> Telefon: 555 555 55 55
+                <FaPhoneAlt className="me-2" style={{ fontSize: '25px' }} />{' '}
+                Telefon: 555 555 55 55
               </span>
               <span>
-                <FaEnvelope className="me-2" style={{fontSize: '25px'}}/> E-posta: mail@mail.com
+                <FaEnvelope className="me-2" style={{ fontSize: '25px' }} />{' '}
+                E-posta: mail@mail.com
               </span>
             </div>
           </div>
