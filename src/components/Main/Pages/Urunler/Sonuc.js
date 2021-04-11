00f1b372df6e76a5f4e84a5180a0f674e85278b3
didactic-goggles <58259532+developerd1320@users.noreply.github.com>
2021-04-11 03:14:56 +0300

    empty result in products page

--- src/components/Main/Pages/Urunler/Sonuc.js
+++ src/components/Main/Pages/Urunler/Sonuc.js
@@ -1,46 +1,48 @@
 import React, { useState, useEffect } from 'react';
+import {FaSearch} from 'react-icons/fa';
 import Product from '../../../UI/Product/Product';
 
 const Sonuc = (props) => {
   console.log('Rendering => Sonuç');
 
   const { filters, allProducts } = props;
-  
+
   const [filteredProducts, setFilteredProducts] = useState([]);
   // const [sortedProducts, setSortedProducts] = useState([]);
   useEffect(() => {
     console.log(allProducts);
-    setFilteredProducts(allProducts.filter((product) => {
-      try {
-        if (
-          filters.categories.length > 0 &&
-          !filters.categories.includes(Number(product.categoryId))
-        )
-          return false;
-        if (
-          filters.subCategories.length > 0 &&
-          !filters.subCategories.includes(Number(product.subCategoryId))
-        )
-          return false;
-        if (
-          filters.childCategories.length > 0 &&
-          !filters.childCategories.includes(Number(product.childCategoryId))
-        )
-          return false;
-        if (
-          filters.brands.length > 0 &&
-          !filters.brands.includes(Number(product.brandId))
-        )
-          return false;
-        if (
-          filters.ratings.length > 0 &&
-          !filters.ratings.includes(Math.round(Number(product.rating)))
-        )
-          return false;
-      } catch (error) {}
-      return true;
-    }));
-
+    setFilteredProducts(
+      allProducts.filter((product) => {
+        try {
+          if (
+            filters.categories.length > 0 &&
+            !filters.categories.includes(Number(product.categoryId))
+          )
+            return false;
+          if (
+            filters.subCategories.length > 0 &&
+            !filters.subCategories.includes(Number(product.subCategoryId))
+          )
+            return false;
+          if (
+            filters.childCategories.length > 0 &&
+            !filters.childCategories.includes(Number(product.childCategoryId))
+          )
+            return false;
+          if (
+            filters.brands.length > 0 &&
+            !filters.brands.includes(Number(product.brandId))
+          )
+            return false;
+          if (
+            filters.ratings.length > 0 &&
+            !filters.ratings.includes(Math.round(Number(product.rating)))
+          )
+            return false;
+        } catch (error) {}
+        return true;
+      })
+    );
   }, [allProducts, filters]);
   // useEffect(() => {
   //   console.log('useEffect');
@@ -62,11 +64,18 @@ const Sonuc = (props) => {
   return (
     <>
       <div className="row">
-        {filteredProducts.map((product) => (
-          <div className="col-md-4 mb-2" key={product.id}>
-            <Product product={product} />
-          </div>
-        ))}
+        {filteredProducts.length < 1 ? (
+          <>
+            <FaSearch className="fs-2 my-3" />
+            <h5 className="text-center">Aradığınız kriterlere uygun ürün bulunamadı</h5>
+          </>
+        ) : (
+          filteredProducts.map((product) => (
+            <div className="col-md-4 mb-2" key={product.id}>
+              <Product product={product} />
+            </div>
+          ))
+        )}
       </div>
     </>
   );
